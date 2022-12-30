5c53e83a88a93d491db038de60df9d0d258f7de8
Waxer59 <78129249+Waxer59@users.noreply.github.com>
2022-12-30 01:16:03 +0100

    fix service worker

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,16 @@
 const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
-      await navigator.serviceWorker.register('/sw.js', {
+      const registration = await navigator.serviceWorker.register('/sw.js', {
         scope: '/',
       });
+      if (registration.installing) {
+        console.log('Service worker installing');
+      } else if (registration.waiting) {
+        console.log('Service worker installed');
+      } else if (registration.active) {
+        console.log('Service worker active');
+      }
     } catch (error) {
       throw new Error('Service worker registration failed');
     }
