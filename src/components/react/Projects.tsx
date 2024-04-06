25d34fffe0d769f9fd771a20d0dc7225812e256b
waxer59 <hnboyano@gmail.com>
2024-04-06 10:58:14 +0200

    add refactor

--- src/components/react/Projects.tsx
+++ src/components/react/Projects.tsx
@@ -3,24 +3,20 @@ import { PROJECTS_DATA } from '../../constants/ProyectsData'
 
 const Projects: React.FC = () => {
   return (
-    <div className="max-w-[1440px] w-11/12 mx-auto">
-      <div id="projects">
-        <h2 className="text-4xl sm:text-5xl font-400 text-center mt-10 mb-24">
-          Projects
-        </h2>
+    <div id="projects">
+      <h2 className="text-4xl sm:text-5xl font-400 text-center">Projects</h2>
 
-        <ul className="p-0 list-none flex flex-wrap justify-evenly mt-0 gap-[25px]">
-          {PROJECTS_DATA.map(({ name, desc, img, links }) => (
-            <ProjectItem
-              desc={desc}
-              img={img}
-              links={links}
-              name={name}
-              key={name}
-            />
-          ))}
-        </ul>
-      </div>
+      <ul className="p-0 list-none flex flex-wrap justify-evenly mt-24 gap-[25px]">
+        {PROJECTS_DATA.map(({ name, desc, img, links }) => (
+          <ProjectItem
+            desc={desc}
+            img={img}
+            links={links}
+            name={name}
+            key={name}
+          />
+        ))}
+      </ul>
     </div>
   )
 }
--- src/components/react/Skillset.tsx
+++ src/components/react/Skillset.tsx
@@ -28,71 +28,67 @@ import {
 
 const Skillset: React.FC = () => {
   return (
-    <div className="max-w-[1440px] w-11/12 mx-auto" id="skillset">
-      <div className="max-w-[1440px] w-11/12 mx-auto">
-        <h2 className="text-4xl sm:text-5xl font-400 text-center mt-[40px] mb-[100px]">
-          Skillset
-        </h2>
+    <div id="skillset">
+      <h2 className="text-4xl sm:text-5xl font-400 text-center">Skillset</h2>
 
-        <ul className="flex flex-col gap-[50px] list-none">
-          <li data-aos="zoom-in-up">
-            <h3 className="border-b-[1px] border-solid border-borderColor p-[15px] font-300 text-xl sm:text-2xl mt-0 mb-[25px]">
-              Languages
-            </h3>
-            <div className="flex flex-wrap gap-[15px]">
-              <JavaScriptIcon />
-              <TypeScriptIcon />
-              <GoIcon />
-              <JavaIcon />
-            </div>
-          </li>
+      <ul className="flex flex-col gap-[50px] list-none mt-24">
+        <li data-aos="zoom-in-up">
+          <h3 className="border-b-[1px] border-solid border-borderColor p-[15px] font-300 text-xl sm:text-2xl mt-0 mb-[25px]">
+            Languages
+          </h3>
+          <div className="flex flex-wrap gap-[15px]">
+            <JavaScriptIcon />
+            <TypeScriptIcon />
+            <GoIcon />
+            <JavaIcon />
+          </div>
+        </li>
 
-          <li data-aos="zoom-in-up">
-            <h3 className="border-b-[1px] border-solid border-borderColor p-[15px] font-300 text-xl sm:text-2xl mt-0 mb-[25px]">
-              Back-end
-            </h3>
-            <div className="flex flex-wrap gap-[15px]">
-              <NodeIcon />
-              <ExpressIcon />
-              <NestjsIcon />
-              <MongoDBIcon />
-              <PostgresDBIcon />
-              <MySqlDBIcon />
-              <FirebaseIcon />
-              <RedisIcon />
-              <SocketIoIcon />
-              <GraphQlIcon />
-            </div>
-          </li>
+        <li data-aos="zoom-in-up">
+          <h3 className="border-b-[1px] border-solid border-borderColor p-[15px] font-300 text-xl sm:text-2xl mt-0 mb-[25px]">
+            Back-end
+          </h3>
+          <div className="flex flex-wrap gap-[15px]">
+            <NodeIcon />
+            <ExpressIcon />
+            <NestjsIcon />
+            <MongoDBIcon />
+            <PostgresDBIcon />
+            <MySqlDBIcon />
+            <FirebaseIcon />
+            <RedisIcon />
+            <SocketIoIcon />
+            <GraphQlIcon />
+          </div>
+        </li>
 
-          <li data-aos="zoom-in-up">
-            <h3 className="border-b-[1px] border-solid border-borderColor p-[15px] font-300 text-xl sm:text-2xl mt-0 mb-[25px]">
-              Front-end
-            </h3>
-            <div className="flex flex-wrap gap-[15px]">
-              <HtmlIcon />
-              <CssIcon />
-              <SassIcon />
-              <BootstrapIcon />
-              <MaterialUiIcon />
-              <TailwindIcon />
-              <ReactIcon />
-              <ReduxIcon />
-            </div>
-          </li>
+        <li data-aos="zoom-in-up">
+          <h3 className="border-b-[1px] border-solid border-borderColor p-[15px] font-300 text-xl sm:text-2xl mt-0 mb-[25px]">
+            Front-end
+          </h3>
+          <div className="flex flex-wrap gap-[15px]">
+            <HtmlIcon />
+            <CssIcon />
+            <SassIcon />
+            <BootstrapIcon />
+            <MaterialUiIcon />
+            <TailwindIcon />
+            <ReactIcon />
+            <ReduxIcon />
+          </div>
+        </li>
 
-          <li data-aos="zoom-in-up">
-            <h3 className="border-b-[1px] border-solid border-borderColor p-[15px] font-300 text-xl sm:text-2xl mt-0 mb-[25px]">
-              DevOps & Testing
-            </h3>
-            <div className="flex flex-wrap gap-[15px]">
-              <DockerIcon />
-              <GitIcon />
-              <JestIcon />
-            </div>
-          </li>
-        </ul>
-      </div>
+        <li data-aos="zoom-in-up">
+          <h3 className="border-b-[1px] border-solid border-borderColor p-[15px] font-300 text-xl sm:text-2xl mt-0 mb-[25px]">
+            DevOps & Testing
+          </h3>
+          <div className="flex flex-wrap gap-[15px]">
+            <DockerIcon />
+            <GitIcon />
+            <JestIcon />
+          </div>
+        </li>
+      </ul>
     </div>
   )
 }
