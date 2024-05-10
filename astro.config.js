b79a633cbc852bcc40c2bf698eaf59fa59fbf3e9
waxer59 <hnboyano@gmail.com>
2024-05-10 03:26:26 +0200

    improve SEO

--- astro.config.js
+++ astro.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 import react from '@astrojs/react'
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
+import sitemap from '@astrojs/sitemap'
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  site: 'https://hgo.one',
+  integrations: [react(), tailwind(), sitemap()]
 })
